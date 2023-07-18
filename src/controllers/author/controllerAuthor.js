50094f561c8adc1938ae216718e536afe3d70fdb
Dionel22 <diondiaz293@gmail.com>
2023-07-18 18:58:18 -0300

    creacion de ruta Author

--- src/controllers/author/controllerAuthor.js
+++ src/controllers/author/controllerAuthor.js
@@ -1,9 +1,16 @@
-const { Autor } = require("../../db")
+const { Author } = require("../../db")
+
+//me traigo todos los autores
 const allAuthor = async () => {
    const author = ["Suzanne Collins", "Stephen King", "Richard Matheson", "Lian Tanner", "Marta Escudero", "Richard Morgan"]
    author.forEach((element)=>{
-    console.log(element)
+   // console.log(element)
+    Author.findOrCreate({
+        where: {name: element}
+      })
    })
+   const response = await Author.findAll()
+   return response;
 }
 
 module.exports = allAuthor;
