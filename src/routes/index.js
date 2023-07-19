7706ad43c22456afdc6a1f2f1d07e3baeff69066
yonatanlla <yonatanllanto174@gmail.com>
2023-07-18 20:23:16 -0500

    Merge origin/dev_ricardo into yonatan_server

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
 const { Router } = require("express");
+<<<<<<< HEAD
+const routesBook = require("./routesBook");
+
 
 const routes = Router();
 
-routes.use("/book", ()=>{})
+routes.use("/book", routesBook);
+=======
+const routerAuthor = require("./routerAutor/routerAuthor");
+const routerGender = require("./routerGender/routerGender");
+
+const routes = Router();
+
+routes.use("/author", routerAuthor);
+routes.use("/gender", routerGender);
+>>>>>>> origin/dev_ricardo
 
 
 module.exports = routes;
