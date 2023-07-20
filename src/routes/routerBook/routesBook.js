9559231f184e5998537d34fcd6e1068ff2c896be
Dionel22 <diondiaz293@gmail.com>
2023-07-20 12:20:59 -0300

    cambio en ruta del detalle

--- src/routes/routerBook/routesBook.js
+++ src/routes/routerBook/routesBook.js
@@ -4,12 +4,13 @@ const {
 	postHandlerBook,
 	deleteHandlerBook,
 	putHandlerBook,
-} = require("../../handles/book/hablderBook");
+} = require("../../handles/book/handlerBook");
 const { getBookById } = require("../../handles/bookById/handlerBookById");
 
 const routesBook = Router();
 
 routesBook.get("/", getHandlerBook);
+routesBook.get("/:id", getBookById);
 routesBook.post("/", postHandlerBook);
 routesBook.delete("/:id", deleteHandlerBook);
 routesBook.put("/:id", putHandlerBook);
