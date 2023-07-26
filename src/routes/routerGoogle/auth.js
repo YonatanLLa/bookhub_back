c804546201594f066560c62860fdd38e596a7aee
Magui Martin <Magali Martin>
2023-07-26 15:37:19 -0300

    cambio callback

--- src/routes/routerGoogle/auth.js
+++ src/routes/routerGoogle/auth.js
@@ -6,7 +6,7 @@ const {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET}= process.env
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3001/auth/google/callback",
+  callbackURL: "https://servidor-libreria.onrender.com/auth/google/callback",
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
