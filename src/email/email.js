15c59476da0925c1f5da3977f43a2224b8dd460f
Dionel22 <diondiaz293@gmail.com>
2023-07-26 11:36:45 -0300

    email

--- src/email/email.js
+++ src/email/email.js
@@ -38,4 +38,40 @@ async function avisoLogin(email) {
     console.error('Error al enviar el correo:', error);
   }
 }
+
+
+async function avisoDeCompra(email, detallesCompra) {
+  const transport = nodemilar.createTransport(config);
+
+  const mensaje = {
+    from: `Tu E-commerce de Libros <${NODEMAILER_MAIL}>`,
+    to: email,
+    subject: 'Confirmación de compra',
+    html: `
+      <div style="font-family: Arial, sans-serif; text-align: center;">
+        <h1 style="color: #1e3799;">¡Gracias por tu compra, ${detallesCompra?.nombreUsuario}!</h1>
+        <p style="font-size: 16px;">Hemos recibido tu orden de compra en nuestro E-commerce de Libros.</p>
+        <h2 style="color: #1e3799;">Detalles de la compra:</h2>
+        <ul style="font-size: 16px; text-align: left;">
+          ${detallesCompra?.map((libro) => `<li>${libro?.titulo} - ${libro?.precio} USD</li>`).join('')}
+        </ul>
+        <p style="font-size: 16px;">Total pagado: ${detallesCompra.priceTotal} USD</p>
+        <p style="font-size: 16px;">Esperamos que disfrutes de tus nuevos libros.</p>
+        <p style="font-size: 16px;">¡Gracias por elegirnos!</p>
+        <p style="font-size: 16px;">Atentamente,</p>
+        <p style="font-size: 16px;">El equipo de Tu E-commerce de Libros</p>
+      </div>
+    `,
+  };
+
+  try {
+    const info = await transport.sendMail(mensaje);
+    console.log('Correo de notificación de compra enviado:', info.messageId);
+    return info;
+  } catch (error) {
+    console.error('Error al enviar el correo de notificación de compra:', error);
+  }
+}
+
+
 module.exports = avisoLogin;
