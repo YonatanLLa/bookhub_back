423a3c38ef4631d62573e8e907d36af72f9f6193
Dionel22 <diondiaz293@gmail.com>
2023-08-03 01:43:38 -0300

    ruta Gender y Author

--- src/controllers/author/controllerAuthor.js
+++ src/controllers/author/controllerAuthor.js
@@ -1,4 +1,4 @@
-const { Author } = require("../../db")
+const { Author, Book } = require("../../db")
 
 //me traigo todos los autores
 const allAuthor = async () => {
@@ -23,7 +23,27 @@ const createAuthor = async (name) => {
    return response;
 }
 
+//actualiza el autor
+const updateAuthor = async (name, newName) => {
+   const response = await Author.findOne({ where: { name } })
+   if (response) {
+      await response.update({name: newName})
+      await Book.update({ AuthorId: response.id }, { where: { AuthorId: response.id } });
+      return true
+   }
+   return false;
+}
+
+//actualiza el autor
+const deleteAuthor = async (name) => {
+   const deletedAuthor = await User.destroy({ where: { name: name} })
+   console.log(deletedAuthor)
+   return deletedAuthor;
+}
+
 module.exports ={ 
    allAuthor,
-   createAuthor
+   createAuthor,
+   updateAuthor,
+   deleteAuthor
 };
--- src/routes/routerAutor/routerAuthor.js
+++ src/routes/routerAutor/routerAuthor.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const {getHandleAllAuthor, postHandleAuthor} = require("../../handlers/author/handleAuthor");
+const {getHandleAllAuthor, postHandleAuthor, putHandleAuthor, deleteHandleAuthor} = require("../../handlers/author/handleAuthor");
 
 const routerAuthor = Router()
 
 routerAuthor.get("/", getHandleAllAuthor)
 routerAuthor.post("/", postHandleAuthor)
+routerAuthor.put("/:name", putHandleAuthor)
+routerAuthor.delete("/:name", deleteHandleAuthor)
 
 module.exports = routerAuthor;
